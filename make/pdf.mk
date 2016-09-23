24ce881a3bf8902d769d0aac35073a58826edc1c
Arne Soete <arne.soete@irc.vib-ugent.be>
2016-09-23 21:43:46 +0200

    init gen pdf

---
+++ make/pdf.mk
@@ -0,0 +1,13 @@
+pdfdestdir  := $(pubdir)/assets/pdf
+pdffile     := $(pdfdestdir)/cursus.pdf
+wkhtmltopdf := /opt/wkhtmltox/bin/wkhtmltopdf
+
+.PHONY: pdf
+pdf: $(pdffile)
+
+$(pdffile): $(shell find $(pubdir)/* -type f) | $(pdfdestdir) $(wkhtmltopdf)
+	$(wkhtmltopdf) toc localhost:8000/cursus.html $@
+
+
+$(pdfdestdir):
+	mkdir -p $@
