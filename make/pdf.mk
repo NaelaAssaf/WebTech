6be5b726af4b4ebb22954edc497b39af57f3a39f
Arne Soete <arne.soete@irc.vib-ugent.be>
2016-10-02 17:03:09 +0200

    fix: also build public pdf

--- make/pdf.mk
+++ make/pdf.mk
@@ -1,13 +1,17 @@
 pdfdestdir  := pdfs
 pdffile     := $(pdfdestdir)/cursus.pdf
+pubpdf      := $(pubdir)/$(pdffile)
 wkhtmltopdf := /opt/wkhtmltox/bin/wkhtmltopdf
 
 .PHONY: pdf
-pdf: $(pdffile)
+pdf: $(pdffile) $(pubpdf)
 
 $(pdffile): $(shell find $(pubdir)/* -type f) make/pdf.mk | $(pdfdestdir) $(wkhtmltopdf)
 	$(wkhtmltopdf) localhost:8000/cursus.html $@
 
+$(pubpdf): $(pdffile)
+	mkdir -p $(dir $@)
+	cp -f $< $@
 
 $(pdfdestdir):
 	mkdir -p $@
