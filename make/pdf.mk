3b53de23e21436eaaff09b3d092cd8c4c1ed5412
Arne Soete <arne.soete@irc.vib-ugent.be>
2018-11-26 10:47:51 +0100

    rebuild pdf

--- make/pdf.mk
+++ make/pdf.mk
@@ -2,12 +2,13 @@ pdfdestdir  := pdfs
 pdffile     := $(pdfdestdir)/cursus.pdf
 pubpdf      := $(pubdir)/$(pdffile)
 wkhtmltopdf := /opt/wkhtmltox/bin/wkhtmltopdf
+wkhtmltopdf := /usr/local/bin/wkhtmltopdf
 
 .PHONY: pdf
 pdf: $(pdffile) $(pubpdf)
 
 $(pdffile): $(shell find $(pubdir)/* -type f -not -name "*.pdf") make/pdf.mk | $(pdfdestdir) $(wkhtmltopdf)
-	$(wkhtmltopdf) localhost:8000/cursus.html $@
+	$(wkhtmltopdf) http://localhost:8000/cursus.html $@
 
 $(pubpdf): $(pdffile)
 	mkdir -p $(dir $@)
